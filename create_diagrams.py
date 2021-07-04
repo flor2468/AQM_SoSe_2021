d7ad0f3ff462fd20893961784a333c22c4028f46
JennyDoering <jenny.doering@uni-weimar.de>
2021-07-04 14:45:28 +0200

    old diagrams file

--- create_diagrams.py
+++ create_diagrams.py
@@ -375,6 +375,39 @@ def create_bar_chart_with_multiple_bars():
     plt.savefig('diagrams/bar_charts/age_motivation.png')
 
     plt.show()
+
+
+# def simple_histogram():
+
+def create_histogram():
+
+    data = readCSV('csv_files/edited/survey_complete.csv')
+    age = processDataToString(data, "How old are you?")
+
+    for i in range(len(age)):
+        age[i] = int(age[i])
+
+    bins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+            31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65]
+    
+    # bins = np.arange(len(bins)) - 0.5
+    hist, bins = np.histogram(age, bins = bins) 
+
+    print(hist)
+    print(bins)
+    fig = plt.figure()
+    from matplotlib.ticker import MaxNLocator
+
+    ax = plt.figure().gca()
+    plt.hist(age, bins = bins, rwidth=0.9, color='turquoise', align='left') 
+    plt.xlim([0, 65])
+    # plt.xticks(np.arange(9,61,1))
+    ax.set_xticks([0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65])
+    ax.set_yticks([0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22])
+
+    plt.savefig('diagrams/histograms/age_histogram.png')
+
+    plt.show()
     
 
 def main():
@@ -393,6 +426,8 @@ def main():
 
     filter_by_age_and_physical_aspects()
     # create_bar_chart_with_multiple_bars()
+
+    create_histogram()
 
 
 if __name__ == main():
